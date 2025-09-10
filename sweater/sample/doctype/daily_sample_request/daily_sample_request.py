c05fed28df8d20f12e9f551a43a67e29eb3a2878
mehedi432 <aamehedi93@gmail.com>
2025-09-11 00:31:59 +0600

    Renaming Daily Sample Reques.

--- sweater/sample/doctype/daily_sample_request/daily_sample_request.py
+++ sweater/sample/doctype/daily_sample_request/daily_sample_request.py
@@ -50,8 +50,8 @@ class DailySampleRequest(Document):
                     item_code_parts.append(row.item_name.upper())
                 if getattr(row, "colour", None):
                     item_code_parts.append(row.colour.upper())
-                if getattr(row, "quantity", None) and row.category.lower() != "yarn":
-                    item_code_parts.append(str(row.quantity))
+                # if getattr(row, "quantity", None) and row.category.lower() != "yarn":
+                #     item_code_parts.append(str(row.quantity))
 
                 # Always append Buyer & Style
                 item_code_parts.extend([self.buyer.upper(), self.style.upper()])
