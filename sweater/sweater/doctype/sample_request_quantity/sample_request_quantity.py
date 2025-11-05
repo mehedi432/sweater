3823d8c24bce144819e9f499c4c043ad624764b3
mehedi432 <aamehedi93@gmail.com>
2025-11-05 22:35:40 +0600

    Removing garbage doctype

--- sweater/sweater/doctype/sample_request_quantity/sample_request_quantity.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Abdullah Al Mehedi and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class SampleRequestQuantity(Document):
-	pass
