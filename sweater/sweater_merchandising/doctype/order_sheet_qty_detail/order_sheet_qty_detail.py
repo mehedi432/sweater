c2101652ee5c15b8423b27be0776bafe1fa6ffb6
mehedi432 <aamehedi93@gmail.com>
2025-11-18 22:59:44 +0600

    Adding Order Sheet Entry working.

---
+++ sweater/sweater_merchandising/doctype/order_sheet_qty_detail/order_sheet_qty_detail.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Abdullah Al Mehedi and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class OrderSheetQtyDetail(Document):
+	pass
