233b1bbc0f638cdf5f1d41893cdb57845736c369
mehedi432 <aamehedi93@gmail.com>
2025-11-19 00:51:59 +0600

    Adding Order sheet

--- sweater/sweater_merchandising/doctype/order_sheet_entry/order_sheet_entry.js
+++ sweater/sweater_merchandising/doctype/order_sheet_entry/order_sheet_entry.js
@@ -6,3 +6,25 @@
 
 // 	},
 // });
+
+frappe.ui.form.on("Order Sheet Yarn Detail", {
+    quantity(frm, cdt, cdn) { calculate_total(frm, cdt, cdn); },
+    consumption(frm, cdt, cdn) { calculate_total(frm, cdt, cdn); },
+    wastage(frm, cdt, cdn) { calculate_total(frm, cdt, cdn); }
+});
+
+function calculate_total(frm, cdt, cdn) {
+    let row = frappe.get_doc(cdt, cdn);
+    let qty = parseFloat(row.quantity) || 0;
+    let cons = parseFloat(row.consumption) || 0; // in grams
+    let wastage = parseFloat(row.wastage) || 0;
+
+    // Total in grams
+    let total_grams = qty * cons * (1 + wastage / 100);
+
+    // Convert to lbs
+    row.total_yarn = total_grams / 453.59237;
+
+    frm.refresh_field("order_sheet_yarn_detail"); // refresh child table
+}
+
